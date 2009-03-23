56458fb6a1a5f9adb0ff27dec903c3ce3525daf2
David J. Kordsmeier <dkords@gmail.com>
2009-03-23 00:34:43 -0700

    Added in support for getting # of commits on master branch over lifetime of project.  Not tested.  Updated conf for needed scm info.

--- src/razortooth/sponge/plugins/GithubDatasourcePlugin.py
+++ src/razortooth/sponge/plugins/GithubDatasourcePlugin.py
@@ -4,8 +4,13 @@ import unittest
 
 class GithubDatasourcePlugin:
     dataDict = {}
+    dataSpec = {} # I'm thinking to use this to define what the data definition looks like
+    workDir = "/tmp"
     #
     # TODO: Define Plugin Metadata
+    #
+    # At this point in time, the github api is limited.  I am trying to find out if there are
+    # more private apis that can be used.
     #
     def __init__(self):
         print "Init GithubDatasourcePlugin..."
@@ -16,8 +21,13 @@ class GithubDatasourcePlugin:
         return {'guid':1,'name':'GithubDataSourcePlugin', 'license':'gplv3'}
     def get_datasource_metadata(self):
         return {'field1':['watchers','integer']}
-    def fetch_data(self):
+    def fetch_data(self, gitcloneurl):
         print "Fetching data"
+        os.execv("git", ["clone", gitcloneurl, "/tmp/githubdatasource.git"])
+        os.chdir("/tmp/githubdatasource.git")
+        f = os.popen('git rev-list --all --reverse')
+        dataDict["totalcommits"] = len(f);
+        os.rmdir("/tmp/githubdatasource.git")
         return dataDict
 class githubDatasourcePluginTests(unittest.TestCase):
     def setUp(self):
